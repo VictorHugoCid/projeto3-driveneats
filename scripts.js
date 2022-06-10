7c91e29c7cafec9e7b029eb03be6266cc6fb217b
VictorHugoCid <victorcidpereira@gmail.com>
2022-06-10 11:13:05 -0300

    Frame 5 terminado 10-06-2022 as 11:15

--- scripts.js
+++ scripts.js
@@ -8,26 +8,28 @@ function pedido(){
 
 function selecaoPratos(item){
     
-    const opcaoSelecionada = document.querySelector(".pratos .selecionado")
+    const opcaoSelecionada = document.querySelector(".pratos .selecionado");
     //console.log(opcaoSelecionada)
 
     if (opcaoSelecionada !== null){
-        opcaoSelecionada.classList.remove("selecionado")
+        opcaoSelecionada.classList.remove("selecionado");
     }
     item.classList.add("selecionado");
     //console.log(item)
+    verificarSelecao()
 }
 
 function selecaoBebidas(item){
     
-    const opcaoSelecionada = document.querySelector(".bebidas .selecionado")
+    const opcaoSelecionada = document.querySelector(".bebidas .selecionado");
     //console.log(opcaoSelecionada)
 
     if (opcaoSelecionada !== null){
-        opcaoSelecionada.classList.remove("selecionado")
+        opcaoSelecionada.classList.remove("selecionado");
     }
     item.classList.add("selecionado");
     //console.log(item)
+    verificarSelecao()
 }
 
 function selecaoSobremesas(item){
@@ -36,8 +38,24 @@ function selecaoSobremesas(item){
     //console.log(opcaoSelecionada)
 
     if (opcaoSelecionada !== null){
-        opcaoSelecionada.classList.remove("selecionado")
+        opcaoSelecionada.classList.remove("selecionado");
     }
     item.classList.add("selecionado");
     //console.log(item)
+    verificarSelecao()
+}
+
+function verificarSelecao(){
+    const prato =  document.querySelector(".pratos .selecionado");
+    const bebida =  document.querySelector(".bebidas .selecionado");
+    const sobremesa =  document.querySelector(".sobremesas .selecionado");
+
+    if (prato !== null && bebida !== null && sobremesa !== null ){
+        const button = document.querySelector(".button")
+        //console.log(button)
+        button.classList.add("fecharPedido")
+        button.innerHTML = "Fechar pedido";
+        
+
+    }
 }
