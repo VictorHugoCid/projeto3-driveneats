e98846fe0328d071977b039913bab4137d1a56c8
VictorHugoCid <victorcidpereira@gmail.com>
2022-06-10 10:22:30 -0300

    Frame 4,5 Seleções 10-06-2022 ad 10h20mim

--- scripts.js
+++ scripts.js
@@ -1,3 +1,43 @@
 function pedido(){
+    /* let btn = document.querySelector(".button");
+    btn.textContent = "Finalizar pedido"; */
+    /* FUNCIONA!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+}
+
+
+
+function selecaoPratos(item){
     
+    const opcaoSelecionada = document.querySelector(".pratos .selecionado")
+    //console.log(opcaoSelecionada)
+
+    if (opcaoSelecionada !== null){
+        opcaoSelecionada.classList.remove("selecionado")
+    }
+    item.classList.add("selecionado");
+    //console.log(item)
+}
+
+function selecaoBebidas(item){
+    
+    const opcaoSelecionada = document.querySelector(".bebidas .selecionado")
+    //console.log(opcaoSelecionada)
+
+    if (opcaoSelecionada !== null){
+        opcaoSelecionada.classList.remove("selecionado")
+    }
+    item.classList.add("selecionado");
+    //console.log(item)
+}
+
+function selecaoSobremesas(item){
+    
+    const opcaoSelecionada = document.querySelector(".sobremesas .selecionado")
+    //console.log(opcaoSelecionada)
+
+    if (opcaoSelecionada !== null){
+        opcaoSelecionada.classList.remove("selecionado")
+    }
+    item.classList.add("selecionado");
+    //console.log(item)
 }
